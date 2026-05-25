6386aa9a73271fb00a119a87a57174d6fc19fd50
altvault <altvault>
2026-05-25 16:02:32 +0700

    add github get asset helper

--- src/altvault/helpers/github.py
+++ src/altvault/helpers/github.py
@@ -3,8 +3,10 @@ import subprocess
 from functools import cache
 from githubkit import GitHub
 from githubkit.auth import ActionAuthStrategy, TokenAuthStrategy
+from pydantic import BaseModel
 
 from altvault.helpers.config import config
+from altvault.helpers.models import GitHubReleasesDebFile
 
 
 @cache
@@ -29,3 +31,34 @@ def _get_local_github_token() -> str:
             f"gh auth token returned an empty token for {config.local_username!r}"
         )
     return token
+
+
+class GitHubReleaseAssetResult(BaseModel):
+    url: str
+    tag: str
+
+
+def get_github_release_asset(
+    info: GitHubReleasesDebFile,
+) -> GitHubReleaseAssetResult:
+    github_client = create_github_client()
+
+    if info.tag == "latest":
+        release = github_client.rest.repos.get_latest_release(
+            owner=info.owner, repo=info.repo
+        )
+    else:
+        release = github_client.rest.repos.get_release_by_tag(
+            owner=info.owner, repo=info.repo, tag=info.tag
+        )
+    if not release:
+        raise ValueError("Release not found")
+    assets = release.parsed_data.assets
+    tag_name = release.parsed_data.tag_name.removeprefix("v")
+    if info.endswith:
+        for asset in assets:
+            if asset.name.endswith(info.endswith):
+                return GitHubReleaseAssetResult(
+                    url=asset.browser_download_url, tag=tag_name
+                )
+    return GitHubReleaseAssetResult(url=assets[0].browser_download_url, tag=tag_name)
