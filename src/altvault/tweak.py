381c10927febdff18ed32b60c211ec2d8a8debe5
altvault <altvault>
2026-05-30 15:41:59 +0700

    custom workaround for ApolloReborn tag

--- src/altvault/tweak.py
+++ src/altvault/tweak.py
@@ -206,6 +206,10 @@ def main():
             injected_filename = f"{app_config.name}_{ipa_download_result.version}_{tweak_config.name}_{tweak_version_label}.ipa"
             tag_name = f"{ipa_download_result.version}_{tweak_version_label}"
 
+        # custom (ApolloReborn puts app_version in their tag_name so prevent duplication)
+        if tweak_version_label.startswith(f"{ipa_download_result.version}_"):
+            tag_name = tag_name.removeprefix(f"{ipa_download_result.version}_")
+
         # release body
         release_body_json = TweakedReleaseBodyJson(
             ipa=TweakedReleaseBodyJson_ipa(
